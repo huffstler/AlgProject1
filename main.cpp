a96c72401f9db2fa23ce5bbf4d60cbdf6104b5eb
Pat <houghstler@gmail.com>
2015-11-09 13:58:34 -0500

    Updated Main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#define WIN32_LEAN_AND_MEAN
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
@@ -24,7 +25,7 @@ public:
 	Node *orgNode;//the originating vertex
 	Node *dstNode;//the destination vertex
 
-//public:
+	//public:
 	Edge(Node *firstNode, Node *secNode)
 	{
 		orgNode = firstNode;
@@ -97,7 +98,7 @@ public:
 		for (int i = 0; i < adjNodeList.size(); i++)
 		{
 			Edge edg = adjNodeList[i];
-			cout <<" -> "<< edg.getDstNode()->getName();
+			cout << " -> " << edg.getDstNode()->getName();
 		}
 
 	}
@@ -153,21 +154,40 @@ public:
 		for (int i = 0; i < nodeList.size(); i++)
 		{
 			nodeList[i]->displayList();
-			cout <<"~~EOL~~"<< endl;
+			cout << "~~EOL~~" << endl;
 		}
 	}
-
 };
+
+
+
+
+
+
+
+
+
+
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void importData();
+string Problem2(string);
+string Problem1(int, string, int);
 Graph G;
+
 
 int main(){
 	importData();
-	system("pause");
+	cout << "What string would like to perfrom problem 2 on" << endl;
+	string x;
+	cin >> x;
+	BFS2(x);
+	//system("pause");
 	return 0;
 }
+
+
 
 void importData(){
 
@@ -183,12 +203,12 @@ void importData(){
 		getline(iss, tempStringP, ':'); // tempString is currently the parent vert
 		Node* PNode = G.findNodeByName(tempStringP);
 		if (PNode){
-		//	cout << "found matching node: " << tempStringP << endl;
+				cout << "found matching node: " << tempStringP << endl;
 		}
 		else{
 			PNode = new Node(tempStringP);
 			G.addNewNode(PNode);
-			//cout << "added Node: " << tempStringP << " as a parent node" << endl;
+			cout << "added Node: " << tempStringP << " as a parent node" << endl;
 			//cout << tempString << endl;
 			count++;
 		}
@@ -196,7 +216,7 @@ void importData(){
 			tempStringC.erase(0, 1);
 			//check if this string is already currently a vert
 			if (Node* N = G.findNodeByName(tempStringC)){
-				//cout << "found matching node: " << tempStringC << endl;
+				cout << "found matching node: " << tempStringC << endl;
 				PNode->addAdjNode(N);
 			}
 			else{
@@ -212,10 +232,91 @@ void importData(){
 			//	cout << tempString << endl;
 		}
 	}
-	//cout << count << endl;
+	cout << count << endl;
 	//G.findNodeByName("artefact")->displayList();
-	string E = G.findNodeByName("artefact")->adjNodeList[0].orgNode->name;
-			cout << E << endl;
-			G.displayGraph();
+	//string E = G.findNodeByName("artefact")->adjNodeList[0].orgNode->name;
+	//cout << E << endl;
+	G.displayGraph();
+	//E.orgNode->getName
 
+	//cout << E << endl;
+
+}
+
+string Problem1(int depth, string s, int amt){
+	Node* r = G.findNodeByName(s);
+	Node* temp = r;
+	for (int currDepth = 0; currDepth < depth; currDepth++){   //one 
+		for (int j = 0; j < r->adjNodeList.size(); j++){		//two
+			if (r->adjNodeList[j].dstNode->adjNodeList.size() >= temp->adjNodeList.size()){ //three
+				temp = r->adjNodeList[j].dstNode;
+			}
+			
+			
+			if ((temp = r->adjNodeList[j].dstNode)->adjNodeList.size() >= amt){
+				temp->addAdjNode;
+			}
+		}
+	}
+}
+
+
+
+string Problem2(string s) {
+	Node* r = G.findNodeByName(s);
+	int max = 0;
+	int numChildren = 0;
+	//bool *visited = new bool[V];
+	string highestNode;
+	// 	'i' will be used to get all adjacent vertices of a vertex
+	list<int>::iterator i;
+
+	//if (maxDepth == 0) { return; }
+
+	//     Mark all the vertices as not visited below
+	G.clearVisited;
+//	for (int i = 0; i < V; i++)
+//		visited[i] = false;
+
+	// 	Create a queue for BFS
+	list<Node*> queue;
+//	list<Node*> depthqueue;
+
+	// 	Mark the current node as visited and enqueue it
+	r->status = VISITED;
+	//visited[s] = true;
+	queue.push_back(r);
+
+	while (!queue.empty()) {
+
+		// 		Dequeue a vertex from queue and print it
+		r = queue.front();
+		queue.pop_front();
+		cout <<"We just popped: "<< r->name << endl;
+
+		// 		Get all adjacent vertices of the dequeued vertex s
+		// 		If a adjacent has not been visited, then mark it visited
+		// 		and enqueue it
+	//	for (i = adj[s].begin(); i != adj[s].end(); ++i) {
+		for (int i = 0; r->adjNodeList.size(); i++){
+		//	for (int i = 0; i < nodeList.size(); i++)
+		//	if (!visited[*i]) {
+			if (r->adjNodeList[i].dstNode->status == NOT_VISITED){
+				numChildren++;
+				r->adjNodeList[i].dstNode->status == VISITED;
+				queue.push_back(r->adjNodeList[i].dstNode);
+
+			}
+		}
+
+		//string q = to_string(s);
+
+		if (numChildren > max){
+			max = numChildren;
+			highestNode = r->name;
+		}
+		numChildren = 0;
+	}
+	cout << highestNode << "   ";
+	return r->name;
 }
